493d6f876cc8d840ded9d940c44cb294a5b8f173
Jana Novotná <jana.posedelova@gmail.com>
2020-05-10 18:18:58 +0200

    Hotová první funkce na zobrazení úkolů z pole tasks

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict';
 
 console.log('funguju!');
+
+const tasks = ['Koupit salát', 'Poslat dopis', 'Zavolat babičce', 'Podívat se na lekci Bootstrapu']
+
+const newTask = document.querySelector('.todo__tasks');
+for (let i = 0; i < tasks.length; i++) {
+  const renderUpdateTask = (item) => {
+    const taskElm = document.createElement('div');
+    taskElm.className = 'task';
+    taskElm.textContent = tasks[i];
+
+    const addNewTask = document.querySelector('.todo__tasks');
+    addNewTask.appendChild(taskElm);
+  };
+  renderUpdateTask(tasks[i]);
+}
